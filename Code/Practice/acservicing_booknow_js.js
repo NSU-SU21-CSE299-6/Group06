1ec975eca5c0a29c763e846af5ab75017c3402b9
ahanik <hannan.anik@northsouth.edu>
2021-08-06 00:36:28 +0600

    Backend #12 : Entered full code of add items
    Every "add items to cart" has separate backend code. That's code are entered.

--- Code/Practice/acservicing_booknow_js.js
+++ Code/Practice/acservicing_booknow_js.js
@@ -4,10 +4,37 @@ function onFormSubmit(){
 
 }
 
+function onFormSubmit1(){
+    var formData = readFormData();
+    insertNewRecord1(formData);
+
+}
+
+function onFormSubmit2(){
+    var formData = readFormData();
+    insertNewRecord2(formData);
+
+}
+
+function onFormSubmit3(){
+    var formData = readFormData();
+    insertNewRecord3(formData);
+
+}
+
 function readFormData(){
 	var formData = {};
-	formData["salary"] = document.getElementById("salary").value;
-	formData["city"] = document.getElementById("city").value;
+	formData["acbasic"] = document.getElementById("acbasic").value;
+	formData["qnt"] = document.getElementById("qnt").value;
+
+    formData["acmaster"] = document.getElementById("acmaster").value;
+    formData["qnt1"] = document.getElementById("qnt1").value;
+
+    formData["acwater"] = document.getElementById("acwater").value;
+    formData["qnt2"] = document.getElementById("qnt2").value;
+
+    formData["acjet"] = document.getElementById("acjet").value;
+    formData["qnt3"] = document.getElementById("qnt3").value;
 	return formData;
 }
 
@@ -15,11 +42,51 @@ function insertNewRecord(data) {
     var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.salary;
+    cell1.innerHTML = data.acbasic;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.city;
+    cell2.innerHTML = data.qnt;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = `<a onClick="onDelete(this)"><i class="fa fa-remove"></a>`;
+    cell3.innerHTML = data.tprice;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = `<a onClick="onDelete(this)"><i class="fa fa-remove"></a>`;
+}
+
+function insertNewRecord1(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.acmaster;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.qnt1;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.tprice;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = `<a onClick="onDelete(this)"><i class="fa fa-remove"></a>`;
+}
+
+function insertNewRecord2(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.acwater;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.qnt2;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.tprice;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = `<a onClick="onDelete(this)"><i class="fa fa-remove"></a>`;
+}
+function insertNewRecord3(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.acjet;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.qnt3;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.tprice;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = `<a onClick="onDelete(this)"><i class="fa fa-remove"></a>`;
 }
 
 function onDelete(td){
